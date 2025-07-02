def45a3d8170a6e720c9e87b4390e77265f67c36
Arjun Vijayvargiya <arjunvijay@google.com>
2025-07-02 16:08:20 +0530

    correcting the auth

--- app-mod-agent/app/agent.py
+++ app-mod-agent/app/agent.py
@@ -19,8 +19,7 @@ from zoneinfo import ZoneInfo
 import google.auth
 from google.adk.agents import Agent
 
-_, project_id = google.auth.default()
-os.environ.setdefault("GOOGLE_CLOUD_PROJECT", project_id)
+os.environ.setdefault("GOOGLE_CLOUD_PROJECT", "agents-stg")
 os.environ.setdefault("GOOGLE_CLOUD_LOCATION", "global")
 os.environ.setdefault("GOOGLE_GENAI_USE_VERTEXAI", "True")
 
