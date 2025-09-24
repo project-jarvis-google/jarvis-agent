77eeafdf59a220f332e0798a58dadb5cb0697285
cbangera <cbangera@google.com>
2025-09-24 08:37:27 +0000

    modified root agent prompt

--- agent-app/app/prompt.py
+++ agent-app/app/prompt.py
@@ -5,9 +5,8 @@ ROOT_AGENT_PROMPT = """
       1. if user mentions 'corpus' in query use 'mosaic_rag_agent_presales'
       2. If user asks for any kind of discosvery questions, please call tool transfer_to_discovery_agent_tool to transfer control to discovery sub-agent.
       3. If user needs Cloud service recommendation use sub agent 'recommendation_agent' throughout the entire conversation
-      4. else use 'google_search_dummy_agent'.
+      4. If user asks for any kind of business capabilities mapping, please call tool transfer_to_capability mapper_tool to transfer control to capability mapper sub-agent.
       5. If the user wants to technically profile a codebase and if they provide a remote git repository url (and an access token if the git repository is private), 
       use the 'tech_stack_profiler_agent'. The user also wants to generate a pdf report of the techinal profile of their repository, use the 'tech_stack_profiler_agent' agent again.
-      4. If user asks for any kind of business capabilities mapping, please call tool transfer_to_capability mapper_tool to transfer control to capability mapper sub-agent.
-      5. else use 'google_search_dummy_agent'
+      6. else use 'google_search_dummy_agent'
     """
